a6dcafba860cdb45267ca19615521be6023e3eeb
kiquetal <kiquetal@gmail.com>
2025-05-11 22:26:19 -0300

    Implement log streaming from Kubernetes pods in TUI

--- internal/tui/logscreen.go
+++ internal/tui/logscreen.go
@@ -1,13 +1,35 @@
 package tui
 
 import (
+	"context"
 	"fmt"
+	"io"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"os"
 )
 
 // DisplayLogsInTUI displays logs in the terminal user interface
 func DisplayLogsInTUI(clientset *kubernetes.Clientset, namespace, podName, containerName string) {
 	fmt.Println("|---------------------------- Pod Logs ------------------------------|")
 	// Fetch and stream logs from Kubernetes
-	kubernetes.StreamPodLogs(clientset, namespace, podName, containerName)
+	err := StreamPodLogs(clientset, namespace, podName, containerName)
+	if err != nil {
+		return
+	}
+}
+func StreamPodLogs(clientset *kubernetes.Clientset, namespace, podName, containerName string) error {
+	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, &v1.PodLogOptions{
+		Container: containerName,
+		Follow:    true,
+	})
+
+	readCloser, err := req.Stream(context.TODO())
+	if err != nil {
+		return err
+	}
+	defer readCloser.Close()
+
+	_, err = io.Copy(os.Stdout, readCloser)
+	return err
 }
